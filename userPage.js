072b5a7ae4e7285212e58fc146821c1f2df98f2c
Rokaskl <rokasklkl@gmail.com>
2019-09-20 21:52:05 +0300

    User can put visit to the back of the queue

--- userPage.js
+++ userPage.js
@@ -1,10 +1,11 @@
 var bt = document.querySelector("#find-user");
 bt.addEventListener("click", () => {
+  updateTable();
+});
+function updateTable() {
   var user = document.querySelector("#name").value;
   var users = [];
   var userVisits = [];
-  output = "";
-  var displayResources = document.querySelector("#tables");
 
   if (typeof window.localStorage.getItem("users") !== undefined) {
     users = JSON.parse(window.localStorage.getItem("users"));
@@ -16,7 +17,13 @@ bt.addEventListener("click", () => {
   });
   console.log(user);
   console.log("found : " + userVisits.length);
+  var displayResources = document.querySelector("#tables");
+
+  var counter;
+  var output = "";
+
   if (userVisits.length > 0) {
+    counter = 0;
     output =
       `<div class="table" id= "${user}-table">` +
       `<H2>${user} visits</H2>` +
@@ -29,12 +36,36 @@ bt.addEventListener("click", () => {
         el.spec +
         "</td><td>" +
         "--:--" +
+        "</td><td>" +
+        ` <button type="button" class="later" id=${counter}> Later</button>` +
         "</td></tr>";
+      counter += 1;
     });
     output += "</table></div>";
     displayResources.innerHTML = output;
-    console.log(output);
+    var laterBtns = document.querySelectorAll(".later");
+    if (laterBtns !== undefined) {
+      laterBtns.forEach(laterBtn => {
+        laterBtn.addEventListener("click", () => {
+          var thisLater = userVisits[laterBtn.getAttribute("id")];
+          MakeItLater(thisLater);
+          updateTable();
+        });
+      });
+    }
   } else {
     displayResources.innerHTML = "<H2 style= 'color : red;'> No visits! <H2> ";
   }
-});
+}
+function MakeItLater(thisLater) {
+  users = JSON.parse(window.localStorage.getItem("users"));
+
+  for (var i = 0; i < users.length; i++) {
+    if (users[i].name == thisLater.name && users[i].spec == thisLater.spec) {
+      users.splice(i, 1);
+      users.push(thisLater);
+      window.localStorage.setItem("users", JSON.stringify(users));
+      return;
+    }
+  }
+}
