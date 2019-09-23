3c39dcf5fdbb85e06a10d4099e9cccdc78ebfeee
Rokaskl <47808459+Rokaskl@users.noreply.github.com>
2019-09-23 23:33:24 +0300

    Update load_dummy_data.js

--- docs/load_dummy_data.js
+++ docs/load_dummy_data.js
@@ -5,30 +5,26 @@ document.querySelector("#load-dummy").addEventListener("click", function() {
     "dummy_visits.json"
   ];
   loadJSON(function(response) {
-    if (response != null) {
+   
       var users = [];
       users = JSON.parse(response);
-
       window.localStorage.setItem("users", response);
     }
   }, files[0]);
   loadJSON(function(response) {
-    if (response != null) {
+   
       var specialists = [];
       specialists = JSON.parse(response);
       window.localStorage.setItem("specialists", response);
-    } else {
-      failedToLoad();
+  
     }
   }, files[1]);
   loadJSON(function(response) {
-    if (response != null) {
+    
       var visits = [];
       visits = JSON.parse(response);
       window.localStorage.setItem("visits", response);
-    } else {
-      failedToLoad();
-    }
+   
   }, files[2]);
 });
 
